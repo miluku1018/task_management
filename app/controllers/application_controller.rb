0c3bd2db785f371d73ff5b3a4b2f0f9b01b00eba
miluku1018 <miluku1018@gmail.com>
2020-02-15 18:36:11 +0800

    add CRUD task 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  private
+    def record_not_found
+      render file: "#{Rails.root}/public/404.html",
+             layout: false,
+             status: 404
+    end
 end
