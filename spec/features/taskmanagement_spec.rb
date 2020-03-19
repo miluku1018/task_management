0010b3f8ed2014129838cd305bd7350ee8f3f2bd
miluku1018 <miluku1018@gmail.com>
2020-03-20 03:03:57 +0800

    update order_by_end_at 3

--- spec/features/taskmanagement_spec.rb
+++ spec/features/taskmanagement_spec.rb
@@ -67,7 +67,8 @@ RSpec.feature "Taskmanagements", type: :feature do
       task2 = Task.create(title: '訂東京奧運門票', description:'買兩張東京奧運門票')
       
       visit root_path
-      
+      click_link I18n.t(:created_at)
+    
       expect(find('table tbody tr:nth-child(1)')).to have_text "訂東京奧運門票"
       expect(find('table tbody tr:nth-child(2)')).to have_text "訂機票"
     end
@@ -79,10 +80,14 @@ RSpec.feature "Taskmanagements", type: :feature do
       task2 = Task.create(title: '取消東京奧運門票', description:'兩張東京奧運門票', end_at: '2020-04-1')
       
       visit root_path
-      click_link I18n.t(:end_at)
-    
+
       expect(find('table tbody tr:nth-child(1)')).to have_text "取消機票"
       expect(find('table tbody tr:nth-child(2)')).to have_text "取消東京奧運門票"
+
+      click_link I18n.t(:end_at)
+      expect(find('table tbody tr:nth-child(1)')).to have_text "取消東京奧運門票"
+      expect(find('table tbody tr:nth-child(2)')).to have_text "取消機票"
+ 
     end
   end
 
